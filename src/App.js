ff2480a65e1c3ba655cf4d686feaa0cb936cacaf
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-09-05 07:54:54 +0200

    fix: netlify build errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-//import { gsap } from 'gsap'
 import '../src/sass/app.scss'
 import Header from './header/Header';
 import Main from './main/Main'
 function App() {  
 
-  /*const tl = gsap.timeline({paused: true})
-  tl.to("Header", {opacity: 1, x: 100, duration: 2})
-
-  window.addEventListener("load", function() {
-    tl.play()
-  })
- */
   return (
     <div className="App">
       <div className="container">
